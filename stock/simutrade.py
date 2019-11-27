fcf4ae914b908e17cde29632428f7295fe3b51fb
iuty <dfdfggg@126.com>
2019-11-27 21:53:22 +0800

    add simutrade

---
+++ stock/simutrade.py
@@ -0,0 +1,71 @@
+from stock.files import SimTradeFile,CqcxFile
+import datetime
+
+class SimuTrader:
+	def __init__(self,code,methodmark,dismark):
+		self._trade = []
+		self._data = []
+		self._file = SimTradeFile(' ',"{}_{}".format(methodmark,dismark))
+		self._file.removeFiles()
+		self._file = SimTradeFile(code,"{}_{}".format(methodmark,dismark))
+		self._cqcxdata = CqcxFile(code).getData()
+		pass
+
+	@property
+	def Hold(self):
+		return len(self._trade) == 2
+
+	@property
+	def Fin(self):
+		return len(self._trade) == 4
+	
+	
+
+	def simuBuy(self,price,daily,tex = 1.0015):
+		if self.Hold:
+			return
+		else:
+			if price < daily[3]:
+				return
+			if price > daily[2]:
+				price = daily[2]
+			self._trade.append(daily[0])
+			self._trade.append(price*tex)
+		pass
+
+	def simuSell(self,price,daily,tex = 0.9975):
+		if not self.Hold:
+			return
+		else:
+			if price > daily[2]:
+				return
+			if price < daily[3]:
+				price = daily[3]
+			self._trade.append(daily[0])
+			self._trade.append(price*tex)
+		pass
+
+	def setParams(self,reason,params):
+		if self.Fin:
+			self._trade.append(reason)
+			self._trade+=params
+			if not self.delCqcx():
+				self._data.append(tuple(self._trade))
+			self._trade = []
+		pass
+
+
+
+	def delCqcx(self):
+		if len(self._trade) >= 4:
+			for cqcx in self._cqcxdata:
+				if (cqcx[0] >= self._trade[0]) & (cqcx[0] <= self._trade[2]):
+					
+					return True
+		return False
+
+	def simuFinish(self):
+		if len(self._data) > 0:
+			self._file.appendData(self._data)
+		pass
+
